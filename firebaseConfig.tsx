20a5218a2e3ffd1461bbf276c83ac661ea9b87e2
ar-aoro <andres@aoro.ai>
2024-09-06 17:10:19 -0500

    se quema el valor del firebase apikey para evitar error con el env en deploy

--- firebaseConfig.tsx
+++ firebaseConfig.tsx
@@ -7,7 +7,7 @@ import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_FIREBASE_API_KEY,
+  apiKey: "AIzaSyDUGmb-97QSHRQGwPoloVZZhd60V37FFe4",
   authDomain: "semana-test.firebaseapp.com",
   projectId: "semana-test",
   storageBucket: "semana-test.appspot.com",
